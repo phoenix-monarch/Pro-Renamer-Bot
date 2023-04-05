0972b506cbc3bd9ac2d80661a1d722ca41548fdb
Mr Killer 1109 <69214079+harshil8981@users.noreply.github.com>
2023-04-06 04:40:03 +0530

    Add files via upload

---
+++ plugins/about.py
@@ -0,0 +1,15 @@
+import os 
+from pyrogram import Client, filters
+token = os.environ.get('TOKEN','')
+botid = token.split(':')[0]
+from helper.database import botdata, find_one, total_user
+
+from helper.progress import humanbytes
+
+@Client.on_message(filters.private & filters.command(["about"]))
+async def start(client,message):
+	botdata(int(botid))
+	data = find_one(int(botid))
+	total_rename = data["total_rename"]
+	total_size = data["total_size"]
+	await message.reply_text(f"Origional BOT :- <a href='http://t.me/Mrkiller_Renamer_Bot'>Mrkiller Renamer Bot</a>\nCreater :- <a href='https://t.me/Hpbot_update'>🦋MrkillerDeveloper🦋</a>\nLanguage :- Python3\nLibrary :- Pyrogram 2.0\nServer :- KOYEB\nTotal Renamed File :- {total_rename}\nTotal Size Renamed :- {humanbytes(int(total_size))} \n\n Thank you **<a href='https://t.me/Mrkiller_1109'>MrkillerDeveloper</a>** for your hard work \n\n❤️ we love you <a href='https://t.me/Mrkiller_1109'>**MrkillerDeveloper**</a> ❤️",quote=True)
